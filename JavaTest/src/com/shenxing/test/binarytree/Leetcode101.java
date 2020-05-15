464f9977d9de57fe071e5cbfd24b4d634ac56099
shenxing <shenxing583@gmail.com>
2020-05-15 09:36:52 +0800

    二叉树 判断树是否是镜像的

--- JavaTest/src/com/shenxing/test/binarytree/Leetcode101.java
+++ JavaTest/src/com/shenxing/test/binarytree/Leetcode101.java
@@ -1,5 +1,8 @@
 package com.shenxing.test.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Leetcode101 {
 
     /**
@@ -33,5 +36,42 @@ public class Leetcode101 {
         }
 
         return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+
+    /**
+     * 对称二叉树
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        // 这里用linklist是因为它可以存放null值
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // 连续取出两个元素比较
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+
+            if (t1.val != t2.val) {
+                return false;
+            }
+
+            // 注意这里的加入队列的顺序
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+
+        }
+
+        return true;
     }
 }
